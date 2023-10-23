d2b2df6ab968933c40d8417ea98801aa774e7739
BogdanStelmah <dubinchenkob@gmail.com>
2023-10-23 20:57:45 +0300

    Updated lab 4 tasks 4 and 8

--- lab4/LR_4_task_4.py
+++ lab4/LR_4_task_4.py
@@ -1,37 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn import datasets
 from sklearn.metrics import mean_squared_error, explained_variance_score
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
-housing_data = datasets.load_boston()
-X, Y = shuffle(housing_data.data, housing_data.target, random_state=7)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=7)
+# Завантаження даних із цінами на нерухомість
+housing_data = datasets.fetch_california_housing()
 
-regressor = AdaBoostRegressor(DecisionTreeClassifier(max_depth=4), n_estimators=400, random_state=7)
-regressor.fit(X_train, Y_train)
+# Перемішування даних
+X, y = shuffle(housing_data.data, housing_data.target, random_state=7)
 
-Y_train_pred = regressor.predict(X_train)
-mse = mean_squared_error(Y_train, Y_train_pred)
-evs = explained_variance_score(Y_train, Y_train_pred)
-print("ADABOOST REGRESSOR")
+# Розбиття даних на навчальний та тестовий набори
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
+
+# Модель на основі регресора AdaBoost
+regressor = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4), n_estimators=400, random_state=7)
+regressor.fit(X_train, y_train)
+
+# Обчислення показників ефективності регресора AdaBoost
+y_pred = regressor.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+evs = explained_variance_score(y_test, y_pred)
+print("\nADABOOST REGRESSOR")
 print("Mean squared error =", round(mse, 2))
 print("Explained variance score =", round(evs, 2))
 
-feature_importance = regressor.feature_importances_
+# Вилучення важливості ознак
+feature_importances = regressor.feature_importances_
 feature_names = housing_data.feature_names
 
-feature_importance = 100.0 * (feature_importance / max(feature_importance))
+# Нормалізація значень важливості ознак
+feature_importances = 100.0 * (feature_importances / max(feature_importances))
 
-index_sorted = np.flipud(np.argsort(feature_importance))
-pos = np.arange(index_sorted.shape[0]) + .5
+# Сортування та перестановка значень
+index_sorted = np.flipud(np.argsort(feature_importances))
 
+# Розміщення міток уздовж осі Х
+pos = np.arange(index_sorted.shape[0]) + 0.5
+
+# Побудова стовпчастої діаграми
 plt.figure()
-plt.bar(pos, feature_importance[index_sorted], align='center')
-plt.xticks(pos, feature_names[index_sorted])
+plt.bar(pos, feature_importances[index_sorted], align='center')
+plt.xticks(pos, [feature_names[i] for i in index_sorted])
 plt.ylabel('Relative Importance')
-plt.title('Variable Importance')
+plt.title('Assessing the importance of features using the AdaBoost regressor')
 plt.show()
--- lab4/LR_4_task_8.py
+++ lab4/LR_4_task_8.py
@@ -3,35 +3,79 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from sklearn import neighbors, datasets
 
+# Завантаження вхідних даних
 input_file = 'data.txt'
 data = np.loadtxt(input_file, delimiter=',')
-X, Y = data[:, :-1], data[:, -1]
+X, y = data[:, :-1], data[:, -1].astype(int)
 
-num_neighbors = 12
-step_size = 0.01
-classifier = neighbors.KNeighborsClassifier(num_neighbors, weights='distance')
-classifier.fit(X, Y)
-
-X_min, X_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-Y_min, Y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-X_values, Y_values = np.meshgrid(np.arange(X_min, X_max, step_size), np.arange(Y_min, Y_max, step_size))
-
-output_mesh = classifier.predict(np.c_[X_values.ravel(), Y_values.ravel()])
-output_mesh = output_mesh.reshape(X_values.shape)
-
+# Відображення вхідних даних на графіку
 plt.figure()
-plt.pcolormesh(X_values, Y_values, output_mesh, cmap=cm.Paired)
-plt.scatter(X[:, 0], X[:, 1], c=Y, s=80, edgecolors='black', linewidth=1, cmap=cm.Paired)
-plt.xlim(X_values.min(), X_values.max())
-plt.ylim(Y_values.min(), Y_values.max())
-plt.title('K Nearest Neighbors classifier on input data')
+plt.title('Input data')
+marker_shapes = 'v^os'
+mapper = [marker_shapes[i] for i in y]
+for i in range(X.shape[0]):
+    plt.scatter(X[i, 0], X[i, 1], marker=mapper[i], s=75, edgecolors='black', facecolors='none')
 
+# Кількість найближчих сусідів
+num_neighbors = 12
+
+# Розмір кроку сітки візуалізації
+step_size = 0.01
+
+# Створення класифікатора на основі методу k найближчих сусідів
+classifier = neighbors.KNeighborsClassifier(num_neighbors, weights='distance')
+
+# Навчання моделі на основі методу k найближчих сусідів
+classifier.fit(X, y)
+
+# Створення сітки для відображення меж на графіку
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+x_values, y_values = np.meshgrid(np.arange(x_min, x_max, step_size), np.arange(y_min, y_max, step_size))
+
+# Виконання класифікатора на всіх точках сітки
+output = classifier.predict(np.c_[x_values.ravel(), y_values.ravel()])
+
+# Візуалізація передбачуваного результату
+output = output.reshape(x_values.shape)
+plt.figure()
+plt.pcolormesh(x_values, y_values, output, cmap=cm.Paired)
+
+# Накладання навчальних точок на карту
+for i in range(X.shape[0]):
+    plt.scatter(X[i, 0], X[i, 1], marker=mapper[i],
+                s=50, edgecolors='black', facecolors='none')
+
+plt.xlim(x_values.min(), x_values.max())
+plt.ylim(y_values.min(), y_values.max())
+plt.title('Classifier model boundaries based on K nearest neighbors')
+
+# Тестування вхідної точки даних
 test_datapoint = [5.1, 3.6]
-plt.scatter(test_datapoint[0], test_datapoint[1], marker='o', s=100, linewidths=3, color='black')
+plt.figure()
+plt.title('Test datapoint')
 
+for i in range(X.shape[0]):
+    plt.scatter(X[i, 0], X[i, 1], marker=mapper[i], s=75, edgecolors='black', facecolors='none')
+
+plt.scatter(test_datapoint[0], test_datapoint[1], marker='x', linewidth=6, s=200, facecolors='black')
+
+# Вилучення K найближчих сусідів
 _, indices = classifier.kneighbors([test_datapoint])
-indices = np.asarray(indices).flatten()
-plt.scatter(X[indices][:, 0], X[indices][:, 1], marker='*', s=80, linewidths=1, color='black', facecolors='none')
-plt.show()
+indices = indices.astype(int)[0]
+
+# Відображення K найближчих сусідів на графіку
+plt.figure()
+plt.title('K Nearest Neighbors')
+
+for i in indices:
+    plt.scatter(X[i, 0], X[i, 1], marker=mapper[y[i]], linewidth=3, s=100, facecolors='black')
+
+plt.scatter(test_datapoint[0], test_datapoint[1], marker='x', linewidth=6, s=200, facecolors='black')
+
+for i in range(X.shape[0]):
+    plt.scatter(X[i, 0], X[i, 1], marker=mapper[i], s=75, edgecolors='black', facecolors='none')
 
 print("Predicted output:", classifier.predict([test_datapoint])[0])
+
+plt.show()
